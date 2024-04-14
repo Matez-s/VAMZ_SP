c7e6e5223183b575fe8fbac6aca259dc27b7ec54
matusiko <kapralik.matus01@gmail.com>
2024-04-14 14:54:16 +0200

    Prihlásiť sa

--- app/src/main/java/com/example/vamz_sp_/MainActivity.kt
+++ app/src/main/java/com/example/vamz_sp_/MainActivity.kt
@@ -1,48 +1,57 @@
 package com.example.vamz_sp_
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.LineHeightStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.vamz_sp_.ui.theme.VAMZ_SP_Theme
-import java.time.format.TextStyle
+
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             VAMZ_SP_Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    Prihlasenie()
-                    Email()
+                    LoginScreen()
                 }
             }
         }
@@ -50,45 +59,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Helloaaaaa $name!",
-        modifier = modifier
-    )
+fun LoginScreen() {
+    var isLogin by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Prihlásiť sa",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "Osoba")
+            }
+            Row(
+                modifier = Modifier.weight(1f),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Switch(
+                    checked = isLogin,
+                    onCheckedChange = { isLogin = it },
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = Color.Yellow,
+                        uncheckedThumbColor = Color.Gray
+                    )
+                )
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Filled.Lock, contentDescription = "Zámok")
+                }
+            }
+        }
+        // Zobrazenie view pre prihlásenie alebo registráciu na základe stavu switchu
+        if (false) {
+            LoginView()
+        } else {
+            SignupView()
+        }
+
+        OutlinedTextField(
+            value = "priklad@email.com...",
+            onValueChange = {},
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = "miri2024",
+            onValueChange = {},
+            label = { Text("Heslo") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Prihlásiť sa")
+        }
+    }
+}
+
+
+
+@Composable
+fun LoginView() {
+    // UI pre prihlásenie
 }
 
 @Composable
-fun Prihlasenie(modifier: Modifier = Modifier) {
-    Text(
-        text = "Prihlásiť sa",
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 132.dp),
-        fontSize = 24.sp,
-        color = Color.Black,
-        textAlign = TextAlign.Center
-    )
-}
-
-@Composable
-fun Email() {
-    Text(
-        text = "Email",
-        modifier = Modifier
-            .width(109.dp)
-            .height(28.dp)
-            .padding(start = 44.dp, top = 259.dp),
-        fontSize = 20.sp,
-        color = Color.Black,
-    )
+fun SignupView() {
+    // UI pre registráciu
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     VAMZ_SP_Theme {
-
-        Prihlasenie()
-        Email()
+        LoginScreen()
     }
 }
